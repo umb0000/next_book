c1121444745567d9b9d0ae9cad1e07ee3b989097
vita <the777the7@gmail.com>
2025-04-28 16:13:17 +0900

    2.5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -9,12 +11,21 @@ export default function App({ Component, pageProps }: AppProps) {
     router.push( "/book/123");
   };  
 
+  useEffect(() => {
+    router.prefetch("/test");},[]);
 
 
 
   return <>
+  <header>
+    <Link href="/">Home</Link>
+    <Link href="/search">search</Link>  
+    <Link href="/book/1">Book 1</Link>
   <div>
     <button onClick={onClickButton}>Go to Book 123</button>
   </div>
+  </header>
+  <Component {...pageProps} />
+
     </>
 }
